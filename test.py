8d1af08065bd0b650197f5ff0341bf52bb8ad849
Mujffar <mujffarsayyad5353@gmail.com>
2023-04-15 00:07:57 +0530

    Update test.py

--- test.py
+++ test.py
@@ -1,13 +1,10 @@
-from flask import Flask, jsonify
-import json
+from flask import Flask
+
 app = Flask(__name__)
-@app.route('/machine-id', methods=['GET'])
-def machine_id():
-    machine_id = get_machine_id()
-    return jsonify(machine_id)
-def get_machine_id():
-    with open('/etc/machine-id', 'r') as f:
-        return {'machine_id': f.read().strip()}
+
+@app.route('/')
+def hello_world():
+    return 'Hello, World!'
+
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
-
