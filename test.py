2755e24af931f415c59f03eb0290f1e635176956
Mujffar <mujffarsayyad5353@gmail.com>
2023-04-14 23:58:45 +0530

    Create test.py

---
+++ test.py
@@ -0,0 +1,12 @@
+from flask import Flask, jsonify
+import json
+app = Flask(__name__)
+@app.route('/machine-id', methods=['GET'])
+def machine_id():
+    machine_id = get_machine_id()
+    return jsonify(machine_id)
+def get_machine_id():
+    with open('/etc/machine-id', 'r') as f:
+        return {'machine_id': f.read().strip()}
+if __name__ == '__main__':
+    app.run()
